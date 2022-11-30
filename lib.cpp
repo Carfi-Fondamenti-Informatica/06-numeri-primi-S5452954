0593202e4edf266431e58af8e3fff12f6f27c346
Giovanni Gemme 5452954 <115399060+S5452954@users.noreply.github.com>
2022-11-30 18:25:26 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+bool numeriprimi (int n) {
 
+    for (int i=2; i<=n; i++) {
+        int divisione;
+        divisione = n/i;
+        if (n % i == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+}
