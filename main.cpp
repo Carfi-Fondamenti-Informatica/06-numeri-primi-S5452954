3524468085f83e02f341f90d79e2aec72c8c3595
Giovanni Gemme 5452954 <115399060+S5452954@users.noreply.github.com>
2022-12-01 20:49:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,10 @@ using namespace std;
 int main(){
     int n=0;
     cin >> n;
-    bool a = numeriprimi (n,2);
-    
-    if (n==97 or n==2 or n==7 or n==53 or n==997) {
+    if (numeriprimi (n,2) == true) {
         cout << "numero primo";
     }
-    else if (n==4 or n==812 or n==42 or n==10 or n==33) {
+    else{
         cout << "numero non primo";
     }
     return 0;
